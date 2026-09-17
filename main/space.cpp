59f51e5442ff003b08218082345a66ac3190cd5b
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-09-17 15:14:24 +0100

    build the intro panel

--- main/space.cpp
+++ main/space.cpp
@@ -4,26 +4,72 @@
 #include <string>
 
 std::string background_image = "../../image/backgroudn.avif";
+
+void setupText(sf::Text& text, sf::Font& font, const std::string& content,sf::Color color){
+    text.setFont(font);
+    text.setString(content);
+    text.setFillColor(color);
+}
+
+struct IntroPanel{
+    sf::RectangleShape restartButton;
+    sf::RectangleShape pauseButton;
+    sf::Text title;
+    sf::Text restartText;
+    sf::Text pauseText;
+};
+
+void setupIntroPanel(IntroPanel& panel, sf::Font &font){
+    const float panelY = 30.f;
+    const float buttonWidth = 100.f;
+    const float buttonHeight = 30.f;
+    const float buttonGap = 20.f;
+
+    // Title
+    setupText(panel.title, font, "tiny physic", sf::Color::Red);
+
+    panel.title.setCharacterSize(24);
+    panel.title.setPosition(20.f, panelY);
+
+
+    // Restart button
+    panel.restartButton.setSize(sf::Vector2f(buttonWidth, buttonHeight));
+    panel.restartButton.setFillColor(sf::Color::Blue);
+    panel.restartButton.setPosition(850.f, panelY);
+
+    setupText(panel.restartText, font, "restart", sf::Color::Black);
+
+    panel.restartText.setCharacterSize(14);
+    panel.restartText.setPosition(870.f, panelY + 6.f);
+
+
+    // Pause button
+    panel.pauseButton.setSize(sf::Vector2f(buttonWidth, buttonHeight));
+
+    panel.pauseButton.setFillColor(sf::Color::Green);
+    panel.pauseButton.setPosition(850.f + buttonWidth + buttonGap, panelY);
+
+    setupText(panel.pauseText, font, "pause", sf::Color::Black);
+
+    panel.pauseText.setCharacterSize(14);
+    panel.pauseText.setPosition(990.f, panelY + 6.f);
+}
+
 void OpenApp(){
     sf::RenderWindow window(sf::VideoMode(1280, 730), "tiny_physic");
 
-    //styling the text
-    sf::Font title_font;
-    title_font.loadFromFile("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
-    sf::Text title;
-    title.setFont(title_font);
-    title.setString("tiny physic");
-    title.setColor(sf::Color::Red);
+    IntroPanel intro;
+    sf::Font font;
+    font.loadFromFile("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
+    setupIntroPanel(intro, font);
 
-    //pause and restart buttons
-    sf::RectangleShape restart_btn(sf::Vector2f(60, 30));
-    restart_btn.setFillColor(sf::Color::Blue);
-    
-
-
-    
     while (window.isOpen()){
         window.clear();
+        window.draw(intro.title);
+        window.draw(intro.pauseButton);
+        window.draw(intro.pauseText);
+        window.draw(intro.restartButton);
+        window.draw(intro.restartText);
         window.display();
     }
     
